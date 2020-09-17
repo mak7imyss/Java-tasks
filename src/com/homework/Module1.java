6b6e1be5bbc9e7b4a135b348484e8b3a8e8e34c5
mak7imyss <mak7imyss@mail.ru>
2020-09-17 19:37:32 +0300

    added 2.2-2.4

--- src/com/homework/Module1.java
+++ src/com/homework/Module1.java
@@ -3,96 +3,82 @@ import java.util.Scanner;
 
     public class Module1 {
         public static void main() {
-            System.out.println("Input number a task: ");
+            System.out.print("Input number a task: ");
             Scanner sc = new Scanner(System.in);
             int task = sc.nextInt();
             int x;
             int y;
-            switch(task) {
-                case 1:
+            switch (task) {
+                case 1 -> {
                     x = sc.nextInt();
                     y = sc.nextInt();
                     System.out.println(Task1(x, y));
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     x = sc.nextInt();
                     y = sc.nextInt();
                     System.out.println(Task2(x, y));
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     int chickens = sc.nextInt();
                     int cows = sc.nextInt();
                     int pigs = sc.nextInt();
                     System.out.println(Task3(chickens, cows, pigs));
-                    break;
-                case 4:
+                }
+                case 4 -> {
                     int prob = sc.nextInt();
                     int prize = sc.nextInt();
                     int pay = sc.nextInt();
                     System.out.println(Task4(prob, prize, pay));
-                    break;
-                case 5:
-                    float a = (float)sc.nextInt();
-                    float b = (float)sc.nextInt();
-                    float c = (float)sc.nextInt();
+                }
+                case 5 -> {
+                    float a = (float) sc.nextInt();
+                    float b = (float) sc.nextInt();
+                    float c = (float) sc.nextInt();
                     System.out.println(Task5(a, b, c));
-                    break;
-                case 6:
+                }
+                case 6 -> {
                     char ch = sc.next().charAt(0);
                     System.out.println(Task6(ch));
-                    break;
-                case 7:
+                }
+                case 7 -> {
                     x = sc.nextInt();
                     System.out.println(Task7(x));
-                    break;
-                case 8:
+                }
+                case 8 -> {
                     x = sc.nextInt();
                     y = sc.nextInt();
                     System.out.println(Task8(x, y));
-                    break;
-                case 9:
+                }
+                case 9 -> {
                     sc.nextLine();
                     String one = sc.nextLine();
                     System.out.println(Task9(one));
-                    break;
-                case 10:
+                }
+                case 10 -> {
                     x = sc.nextInt();
                     y = sc.nextInt();
                     int z = sc.nextInt();
                     System.out.println(Task10(x, y, z));
+                }
             }
 
         }
-
         public static int Task1(int x, int y) {
-            int f = x % y;
-            return f;
+            return x % y;
         }
-
         public static float Task2(int x, int y) {
-            float s = (float)((double)(x * y) * 0.5D);
-            return s;
+            return (float) ((x * y) * 0.5D);
         }
-
         public static int Task3(int x, int y, int z) {
             int legs_chickens = 2;
             int legs_cows = 4;
             int legs_pigs = 4;
-            int s = legs_chickens * x + legs_cows * y + legs_pigs * z;
-            return s;
+            return legs_chickens * x + legs_cows * y + legs_pigs * z;
         }
-
         public static boolean Task4(int x, int y, int z) {
-            boolean flag;
-            if (x * y > z) {
-                flag = true;
-            } else {
-                flag = false;
-            }
-
-            return flag;
+             return x * y > z;
         }
-
         public static String Task5(float a, float b, float c) {
             String operator;
             if (a - b == c) {
@@ -109,11 +95,9 @@ import java.util.Scanner;
 
             return operator;
         }
-
         public static int Task6(char ch) {
             return ch;
         }
-
         public static int Task7(int x) {
             int s = 0;
 
@@ -123,25 +107,18 @@ import java.util.Scanner;
 
             return s;
         }
-
         public static int Task8(int x, int y) {
             return x + y - 1;
         }
-
         public static int Task9(String one) {
             String[] list = one.split(" ");
             int s = 0;
-            String[] var3 = list;
-            int var4 = list.length;
-
-            for(int var5 = 0; var5 < var4; ++var5) {
-                String value = var3[var5];
-                s = (int)((double)s + Math.pow((double)Integer.parseInt(value), 3.0D));
+            for (String value : list) {
+                s += (int) Math.pow(Integer.parseInt(value), 3.0D);
             }
 
             return s;
         }
-
         public static boolean Task10(int a, int b, int c) {
             int s = a;
 
--- src/com/homework/Module2.java
+++ src/com/homework/Module2.java
@@ -4,47 +4,45 @@ import java.util.Scanner;
 
 public class Module2 {
     public static void main() {
-        System.out.println();
+
         System.out.print("Input number a task: ");
         Scanner sc = new Scanner(System.in);
         int task = sc.nextInt();
         String str;
         int a;
-        switch(task) {
-            case 1:
+        switch (task) {
+            case 1 -> {
                 str = sc.next();
                 a = sc.nextInt();
-                for (String value:Task1(str,a)){
+                for (String value : Task1(str, a)) {
                     System.out.print(value);
-                };
-                break;
-            case 2:
-                System.out.println();
-                break;
-            case 3:
-                System.out.println();
-                break;
-            case 4:
-                System.out.println();
-                break;
-            case 5:
-                System.out.println();
-                break;
-            case 6:
-                System.out.println();
-                break;
-            case 7:
-                System.out.println();
-                break;
-            case 8:
-                System.out.println();
-                break;
-            case 9:
-                System.out.println();
-                break;
-            case 10:
-                System.out.println();
-                break;
+                }
+            }
+            case 2 -> {
+                sc.nextLine();
+                str = sc.nextLine();
+                System.out.println(Task2(str));
+            }
+            case 3 -> {
+                sc.nextLine();
+                str = sc.nextLine();
+                System.out.println(Task3(str));
+            }
+            case 4 -> {
+                sc.nextLine();
+                str = sc.nextLine();
+                System.out.println(Task4(str));
+            }
+            case 5 -> {
+                sc.nextLine();
+                str = sc.nextLine();
+                System.out.println(Task5(str));
+            }
+            case 6 -> System.out.println();
+            case 7 -> System.out.println();
+            case 8 -> System.out.println();
+            case 9 -> System.out.println();
+            case 10 -> System.out.println();
         }
 
     }
@@ -58,4 +56,46 @@ public class Module2 {
         //можно было проще, просто вывести жлементы массива по порядку, но мне захотелось хранить значения для выхода
         return answer;
     }
+    public static int Task2(String str){
+        String[] list = str.split(" ");
+        int[] answer = new int[list.length];
+        for (int i=0;i<list.length;i++){
+            answer[i] = Integer.parseInt(list[i]);
+        }
+        int max = answer[0];
+        int min = answer[0];
+        for(int i = 0; i != answer.length; i ++){
+            if(answer[i] > max){
+                max = answer[i];
+            }
+            if(answer[i] < min){
+                min = answer[i];
+            }
+        }
+        return max-min;
+    }
+    public static boolean Task3(String str){
+        String[] list = str.split(" ");
+        int[] answer = new int[list.length];
+        int s=0;
+        for (int i=0;i<list.length;i++){
+            answer[i] = Integer.parseInt(list[i]);
+            s += Integer.parseInt(list[i]);
+        }
+        return s % list.length==0;
+    }
+    public static String Task4(String str){
+        String[] list = str.split(" ");
+        int[] answer = new int[list.length];
+        answer[0]=answer[0] = Integer.parseInt(list[0]);
+        for (int i=1;i<list.length;i++){
+            answer[i] = Integer.parseInt(list[i])+answer[i-1];
+        }
+        return Arrays.toString(answer);
+    }
+    public static float Task5(String str){
+        float fl = Float.parseFloat(str);
+        return fl%1%1;
+    }
+
 }
