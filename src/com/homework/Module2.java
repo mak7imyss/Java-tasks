e1d601f86658e63542505dd034bc8fd5e1eb6033
mak7imyss <mak7imys@mail.ru>
2020-09-18 11:48:22 +0300

    completed module 2

--- src/com/homework/Module2.java
+++ src/com/homework/Module2.java
@@ -1,6 +1,7 @@
 package com.homework;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Module2 {
     public static void main() {
@@ -8,10 +9,11 @@ public class Module2 {
         System.out.print("Input number a task: ");
         Scanner sc = new Scanner(System.in);
         int task = sc.nextInt();
-        String str;
+        String str, str1;
         int a;
         switch (task) {
             case 1 -> {
+                System.out.print("Введите через пробел слово и число повторений символов:");
                 str = sc.next();
                 a = sc.nextInt();
                 for (String value : Task1(str, a)) {
@@ -19,30 +21,60 @@ public class Module2 {
                 }
             }
             case 2 -> {
+                System.out.print("Введите через пробел массив чисел: ");
                 sc.nextLine();
                 str = sc.nextLine();
                 System.out.println(Task2(str));
             }
             case 3 -> {
+                System.out.print("Введите через пробел массив чисел: ");
                 sc.nextLine();
                 str = sc.nextLine();
                 System.out.println(Task3(str));
             }
             case 4 -> {
+                System.out.print("Введите через пробел массив чисел: ");
                 sc.nextLine();
                 str = sc.nextLine();
                 System.out.println(Task4(str));
             }
             case 5 -> {
+                System.out.print("Введите число: ");
                 sc.nextLine();
                 str = sc.nextLine();
                 System.out.println(Task5(str));
             }
-            case 6 -> System.out.println();
-            case 7 -> System.out.println();
-            case 8 -> System.out.println();
-            case 9 -> System.out.println();
-            case 10 -> System.out.println();
+            case 6 -> {
+                System.out.print("Введите число: ");
+                a = sc.nextInt();
+                System.out.println(Task6(a));
+            }
+            case 7 -> {
+                System.out.print("Введите 5 чисел: ");
+                sc.nextLine();
+                str = sc.nextLine();
+                System.out.println(Task7(str));
+            }
+            case 8 -> {
+                sc.nextLine();
+                System.out.print("Введите через пробел пару слов: ");
+                str = sc.next();
+                str1 = sc.next();
+                System.out.println(Task8(str, str1));
+            }
+            case 9 -> {
+                // Немного изменил задание, сделав одну функцию, которая возвращает название требуемой функции из задания, но суть та же
+                System.out.print("Введите через пробел слово и префикс(суффикс): ");
+                sc.nextLine();
+                str = sc.next();
+                str1 = sc.next();
+                System.out.println(Task9(str,str1));
+            }
+            case 10 -> {
+                System.out.print("Введите номер шага: ");
+                a = sc.nextInt();
+                System.out.println(Task10(a));
+            }
         }
 
     }
@@ -53,7 +85,7 @@ public class Module2 {
             answer[i] = list[i/a];
 
         }
-        //можно было проще, просто вывести жлементы массива по порядку, но мне захотелось хранить значения для выхода
+        //можно было проще, просто вывести элементы массива по порядку, но мне захотелось хранить значения для выхода
         return answer;
     }
     public static int Task2(String str){
@@ -93,9 +125,64 @@ public class Module2 {
         }
         return Arrays.toString(answer);
     }
-    public static float Task5(String str){
-        float fl = Float.parseFloat(str);
-        return fl%1%1;
+    public static int Task5(String str){
+        int index = str.indexOf(".");
+        if (index!=-1){
+            return Integer.parseInt(str.substring(index+1,index+2));
+        }
+        else{
+            return 0;
+        }
     }
-
+    public static int Task6(int a){
+        int[] f = new int[Math.abs(a)+1];
+        f[0] = 0;
+        if (a!=0) f[1] = 1;
+        for (int i = 2; i <= Math.abs(a); ++i) {
+            f[i] = f[i - 1] + f[i - 2];
+            }
+        if (a<0) f[Math.abs(a)]= (int) (Math.pow(-1,(a+1))*f[Math.abs(a)]);
+        //условие не дает полного понимая что должно получиться, числа Фибоначчи считаеются с 0, который принимается за 0 порядок и 3 число будет 2 (0{0} 1{1} 1{2} [2]{3} 3{4} 5 8 ...)
+        //https://ru.wikipedia.org/wiki/%D0%A7%D0%B8%D1%81%D0%BB%D0%B0_%D0%A4%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8
+        return f[Math.abs(a)];
+    }
+    public static boolean Task7(String str) {
+        return str.length()<=5 && str.matches("[-+]?\\d+");
+    }
+    public static boolean Task8(String str, String str1){
+        return (str.length()==0 && str1.length()==0) || (str.charAt(0)==str1.charAt(str1.length()-1)) && (str.charAt(str.length()-1)==str1.charAt(0));
+    }
+    public static String Task9(String str, String str1){
+        String[] word = str1.split("-");
+        if (str1.charAt(0)== '-'){
+            String r = ".*"+word[1]+"$";
+            if (Pattern.matches(r,str)){
+                return "Suffix true";
+            }
+            else {
+                return "Suffix false";
+            }
+        }
+        else if (str1.charAt(str1.length()-1)=='-'){
+            String r = "^"+word[0]+".*";
+            if (Pattern.matches(r,str)){
+                return "Prefix true";
+            }
+            else {
+                return "Prefix false";
+            }
+        }
+        return "N/a";
+    }
+    public static int Task10(int a){
+        if (a%2!=0){
+            return a + 2;
+        }
+        else if (a<0){
+            return 0;
+        }
+        else{
+            return a;
+        }
+    }
 }
